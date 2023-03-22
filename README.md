7773b8dd20461bbc79ea10cfed7ecba1dab17155
Elen Eisendle <ln@calcifer.ee>
2023-03-22 11:12:00 +0100

    add update_packs.py script

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ This repository contains SVD files for the MSPM0L and MSPM0G series of Cortex-M0
 
 The goal of this project is mostly to keep track of changes and provide a source of patched SVDs which can then be used to build
 rust PACs with svd2rust or zig files with regz, but is not tied to either.
+
+The SVD files are from the Keil pack files, which in turn link to TI.
+
+In case the SVD files are out of date, the can simply be updated by running
+
+```sh
+./scripts/update_packs.py
+```
+
+The script checks the current version and fetches and extracts newer pack files automatically if the version number changed.
+The version in the script should be manually updated in case of an update then.
---
+++ scripts/update_packs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import io
+import os
+import urllib.request
+import xml.etree.ElementTree as ET
+from zipfile import ZipFile
+
+packs = {"MSPM0G_DFP": "1.0.0", "MSPM0L_DFP": "1.0.0"}
+
+
+def process_pack(e: ET.Element):
+    url = f"{e.attrib['url']}{e.attrib['vendor']}.{e.attrib['name']}.{e.attrib['version']}.pack"
+
+    with urllib.request.urlopen(url) as f:
+        buf = f.read()
+
+    f = io.BytesIO(buf)
+    ar = ZipFile(f)
+
+    # extract all svd files
+    for item in ar.infolist():
+        if item.filename.endswith(".svd"):
+            item.filename = os.path.basename(item.filename).lower()
+            print(f"{item.filename}, {item.file_size}B")
+            ar.extract(item, "svd")
+
+
+with urllib.request.urlopen(
+    "https://sadevicepacksprodus.blob.core.windows.net/idxfile/index.pidx"
+) as f:
+    text = f.read().decode("utf-8")
+
+root = ET.fromstring(text)
+pindex = root.find("pindex")
+for pdsc in pindex:
+    name = pdsc.attrib["name"]
+    version = pdsc.attrib["version"]
+
+    if name in packs:
+        if packs[name] < pdsc.attrib["version"]:
+            print(f"fetching {name}")
+            process_pack(pdsc)
+            print(f"new version {version} for {name} extracted")
+        else:
+            print(f"same version for {name}")
